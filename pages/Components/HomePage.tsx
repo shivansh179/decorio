3ce891e371ef458a097cbd0d3dd1195411731ad2
shivansh179 <prasantshukla89@gmail.com>
2025-06-18 23:50:49 +0530

    added two pages that is home and contact

--- pages/Components/HomePage.tsx
+++ pages/Components/HomePage.tsx
@@ -285,7 +285,7 @@ const DesignCafeHomepage: React.FC = () => {
       </header>
 
       {/* Hero Section */}
-      <section className="relative min-h-screen bg-gradient-to-br from-orange-50 via-white to-red-50 overflow-hidden">
+      <section className="relative min-h-screen bg-gradient-to-br from-orange-100 via-white to-red-50 overflow-hidden">
         <div className={`absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23f97316" fill-opacity="0.05"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-50`}></div>
         
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative">
